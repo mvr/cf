194844dd5d32c4a9134aadb41bbaf8d4d203a60b
Mitchell Riley <mitchell.v.riley@gmail.com>
2015-02-01 11:28:40 +1000

    Add some simple sanity tests

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,12 +1,21 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
 
+import Data.Maybe
+
 import Math.ContinuedFraction.Effective
+import Math.ContinuedFraction.Interval
 
 import Test.QuickCheck
 import Test.QuickCheck.Function
 import Test.Framework.TH
 import Test.Framework.Providers.QuickCheck2
+
+prop_sensibleEmittable x = isJust $ existsEmittable (primitiveBound x)
+  where types = x :: Integer
+
+prop_sensiblePrimitiveBound x = fromInteger x `elementOf` primitiveBound x
+  where types = x :: Integer
 
 main :: IO ()
 main = $defaultMainGenerator
