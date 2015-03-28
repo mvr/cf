69ed112fc267195a125c91cd510e91e73ba163e7
Mitchell Riley <mitchell.v.riley@gmail.com>
2015-03-28 18:30:57 +1000

    Interpret (Infinity, Infinity) as a singleton interval

--- src/Math/ContinuedFraction/Interval.hs
+++ src/Math/ContinuedFraction/Interval.hs
@@ -55,8 +55,8 @@ instance (Show a) => Show (Extended a) where
   show Infinity = "Infinity"
 
 instance (Num a, Ord a) => Ord (Interval a) where
-  Interval _ _ <= Interval Infinity Infinity = True
-  Interval Infinity Infinity <= Interval _ _ = False
+  Interval _ _ <= Interval Infinity Infinity = False -- TODO CHECK
+  Interval Infinity Infinity <= Interval _ _ = True
   Interval i1 s1 <= Interval i2 s2 =    (i1 <= s1 && i2 <= s2 && s1 - i1 <= s2 - i2)
                                      || (i1 >  s1 && i2 >  s2 && i1 - s1 >= i2 - s2)
                                      || (i1 <= s1 && i2 >  s2)
